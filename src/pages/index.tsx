a8d2583b0ca6883d9cf260d2ee9cde6e32760e3c
Abrar-ul-Haq <96546718+abrarulhaq7@users.noreply.github.com>
2023-10-24 19:56:00 +0500

    staticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import DashboardLayout from 'src/layouts/dashboard/DashboardLayout';
 import LandingPage from 'src/components/dashboard/LandingPage';
 import AboutPage from 'src/components/dashboard/AboutPage';
+import { GetStaticProps } from 'next';
 
 // ----------------------------------------------------------------------
 
@@ -14,10 +15,37 @@ export default function Index() {
   return (
     <>
       <Head>
-        <title>Ice Breaker | Dashboard</title>
+        <title>Ice Breaker</title>
+        <meta property="og:title" content="Ice Breaker" />
+        <meta
+          property="og:description"
+          content="Join our Ice Breaker community and break the ice with those peoples who are just a stone's away and share your enthusiasm with similar favorite topics!"
+        />
+        <meta property="og:url" content="https://icebreaker-website-ts.vercel.app/" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dsvcw2svb/image/upload/v1698158483/Ice-breaker-website/v1grm02lbkkip6cxbu2b.png"
+        />
       </Head>
       <LandingPage />
       <AboutPage />
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    return {
+      props: {
+        notFound: true,
+        data: [],
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+};
